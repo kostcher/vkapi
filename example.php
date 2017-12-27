c3f313fc3410661911df97509833c7c4ac98ccb5
K Cherepanov <k.s.cherepanov@gmail.com>
2017-12-27 16:23:42 +0300

    update

--- example.php
+++ example.php
@@ -1,13 +1,15 @@
 <?php
 require_once (__DIR__ . "/vendor/autoload.php");
 
-$token = "my_token";
+$token = "token";
 
 $params = [
     'message' => "ping",
     'user_id' => "203268308",
 ];
 
-$resutl = VK\VKGroupsApi::send("get", "messages.send", $token, $params);
+$vk = new \VK\VKGroupsApi($token);
+
+$resutl = $vk->request("messages.send", $params);
 
 print_r($resutl);
--- src/VKGroupsApi.php
+++ src/VKGroupsApi.php
@@ -11,22 +11,8 @@ class VKGroupsApi
     /**
      * @var array
      */
-    private $allow_request_methods = ["get", "post"];
-
-    /**
-     * @var
-     */
-    public $request_method;
-
-    /**
-     * @var array
-     */
     private $params = [];
 
-    /**
-     * @var null
-     */
-    private static $instance = null;
 
     const API_URL = 'https://api.vk.com/method/';
 
@@ -34,25 +20,9 @@ class VKGroupsApi
      * VKGroupsApi constructor.
      * @param $token
      */
-    private function __construct($token)
+    public function __construct($token)
     {
         $this->token = $token;
-    }
-
-    /**
-     * Устанавливает метод обращения к api
-     * @param $request_method
-     * @return $this
-     * @throws \Exception
-     */
-    private function setRequestMethod($request_method)
-    {
-        if (in_array($request_method, $this->allow_request_methods)) {
-            $this->request_method = $request_method;
-            return $this;
-        }
-
-        throw new \Exception('Request method not alllow');
     }
 
     /**
@@ -60,7 +30,7 @@ class VKGroupsApi
      * @param $params
      * @return $this
      */
-    private function setParams($params)
+    public function setParams($params)
     {
         $this->params = $params;
         $this->params["access_token"] = $this->token;
@@ -68,59 +38,21 @@ class VKGroupsApi
         return $this;
     }
 
+
     /**
-     * Для методов API требующих обращение через GET
      * @param $api_method
+     * @param $params
      * @return array|object|string
      */
-    private function get($api_method)
+    public function request($api_method, $params)
     {
+        $this->setParams($params);
+
         $response = \Httpful\Request::get(
             self::API_URL . $api_method . "?" . http_build_query($this->params)
         )
             ->send();
 
         return $response->body;
-    }
-
-    /**
-     * Для методов API требующих обращение через POST
-     * @param $api_method
-     * @param $attach
-     * @return array|object|string
-     */
-    private function post($api_method, $attach)
-    {
-        $response = \Httpful\Request::post(
-            self::API_URL . $api_method
-        )
-            ->body($params)
-            ->sendsType(\Httpful\Mime::FORM)
-            ->attach($attach)
-            ->send();
-
-        return $response->body;
-    }
-
-    /**
-     * @param $request_method
-     * @param $api_method
-     * @param $token
-     * @param $params
-     * @param array $attachment
-     * @return mixed
-     */
-    public static function send($request_method, $api_method, $token, $params, $attachment = [])
-    {
-        if (self::$instance === null) {
-            self::$instance = new VKGroupsApi($token);
-        }
-
-        self::$instance
-            ->setRequestMethod($request_method)
-            ->setParams($params);
-
-
-        return self::$instance->{self::$instance->request_method}($api_method, $attachment);
     }
 }
